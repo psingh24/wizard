cebe6a4a1e83afcd4d33d1e38c2de2d87e9b1e74
ikliptic <adrian@ikliptic.com>
2018-04-24 13:05:17 -0500

    Prettify

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,43 +1,63 @@
+import { HTML5_FMT } from "moment";
+
 var React = require("react");
 var createReactClass = require("create-react-class");
 var Nav = require("./Nav");
 var Link = require("react-router").Link;
 
-
-
 var Footer = createReactClass({
- 
-    handleContinue() {
-        event.preventDefault();
-        this.props.continue(1, this.props.page);
-    },
-    handleBack() {
-        event.preventDefault();
-        this.props.back(1);
-    },
-    render: function() {
+  handleContinue() {
+    event.preventDefault();
+    this.props.continue(1, this.props.page);
+  },
+  handleBack() {
+    event.preventDefault();
+    this.props.back(1);
+  },
+  render: function() {
     return (
+      <div className="footer">
+        <div className="event-back">
+          {this.props.page > 0 ? (
+            <a onClick={this.handleBack} href="#" className="footer-btn">
+              <span className="chevron-event">
+                <img
+                  src="assets/image/icon-chevron-left.svg"
+                  className="chevron"
+                  alt=""
+                />
+              </span>{" "}
+              Back
+            </a>
+          ) : (
+            <div />
+          )}
+        </div>
 
-        <div className="footer">
-            <div className="event-back">
+        <div>
+          <p className="errorMessage">{this.props.errorMessage}</p>
+        </div>
 
-            {this.props.page > 0
-            ? <a onClick={this.handleBack} href="#" className="footer-btn"><span className="chevron-event"><img src="assets/image/icon-chevron-left.svg" className="chevron" alt=""/></span> Back</a>
-            : <div></div>}
-            </div>
-
-            <div><p className="errorMessage">{this.props.errorMessage}</p></div>
-            {/* <div className="event-forward">
-                <a className="footer-btn" onClick={this.handleContinue} href="#">Continue <span className="chevron-event"><img src="assets/image/icon-chevron-right.svg" className="chevron" alt=""/></span></a>
-            </div> */}
-
-        {this.props.page > 9 ? "" : <div className="event-forward">
-                <a className="footer-btn" onClick={this.handleContinue} href="#">Continue <span className="chevron-event"><img src="assets/image/icon-chevron-right.svg" className="chevron" alt=""/></span></a>
-            </div> } 
-           {/* <div className="event-forward">
+        {this.props.page > 9 ? (
+          ""
+        ) : (
+          <div className="event-forward">
+            <a className="footer-btn" onClick={this.handleContinue} href="#">
+              Continue{" "}
+              <span className="chevron-event">
+                <img
+                  src="assets/image/icon-chevron-right.svg"
+                  className="chevron"
+                  alt=""
+                />
+              </span>
+            </a>
+          </div>
+        )}
+        {/* <div className="event-forward">
                 <a className="footer-btn" onClick={this.handleContinue} href="#">Continue <span className="chevron-event"><img src="assets/image/icon-chevron-right.svg" className="chevron" alt=""/></span></a>
             </div>  */}
-        </div>
+      </div>
     );
   }
 });
