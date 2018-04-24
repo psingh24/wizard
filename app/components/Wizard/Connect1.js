5684a219087016265a7e30ed988f9cdeb30b40b6
ikliptic <adrian@ikliptic.com>
2018-04-24 10:16:37 -0500

    update className

--- app/components/Wizard/Connect1.js
+++ app/components/Wizard/Connect1.js
@@ -16,16 +16,16 @@ var Connect1 = createReactClass({
 
         <div className="checkBox01"><Checkbox></Checkbox></div>
         <div className="checkBox01Text">Connect your VitalBand to the charger</div>
-        <div className="checkBox01Image"><Image src="assets/image/watch-with-charger.png" class="connectThumb checkBox01Image"/></div>
+        <div className="checkBox01Image"><Image src="assets/image/watch-with-charger.png" className="connectThumb checkBox01Image"/></div>
 
         <div className="checkBox02"><Checkbox></Checkbox></div>
         <div><p className="checkBox02Text">Plug the power bale into the back of your Vitalhub</p></div>
-        <div className="checkBox02Image"><Image src="assets/image/Pi-with-charge.png" class="connectThumb checkBox02Image"/></div>
+        <div className="checkBox02Image"><Image src="assets/image/Pi-with-charge.png" className="connectThumb checkBox02Image"/></div>
 
 
         <div className="checkBox03"><Checkbox></Checkbox></div>
         <div className="checkBox03Text">Plug your VitalBand into your router wiht the ethernet cord</div>
-        <div className="checkBox03Image"><Image src="assets/image/Pi-with-cable.png" class="connectThumb checkBox03Image"/></div>
+        <div className="checkBox03Image"><Image src="assets/image/Pi-with-cable.png" className="connectThumb checkBox03Image"/></div>
          
      </div>
         
--- app/components/Wizard/Connect4.js
+++ app/components/Wizard/Connect4.js
@@ -9,7 +9,7 @@ var Connect4 = createReactClass({
   render: function() {
     return (
      <div>
-         <Image src="assets/image/smartphones.svg" class="imageSmartPhone"/>
+         <Image src="assets/image/smartphones.svg" className="imageSmartPhone"/>
          <h1>From your moble device go to the application store and download the VitalBand application. Enter your username and password to finish registering.</h1>
          <h1>Once you have successfully logges in to the mobile application select continue. If you don't want to use the mobile feature you can select skip this step.</h1>
          <div className="noEthernetBTN"><Button className="btnLarge" bsStyle="primary">SKIP THIS STEP</Button></div>
